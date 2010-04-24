a365cf2390183265f39b9b6f8e11e6c484517f83
Mike Norman <mwnorman@gmail.com>
2010-04-23 20:05:18 -0400

    Add org.eclipse.core.expressions.propertyTesters extension  - create DbConnectionTester: returns true/false depending on state of    AnalysisDatabase's databaseSession  - use property as part of a <test> condition for definition    isConnectedDatabase/isDisconnectedDatabase

---
+++ src/ca/carleton/tim/ksat/client/DbConnectionTester.java
@@ -0,0 +1,23 @@
+package ca.carleton.tim.ksat.client;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+public class DbConnectionTester extends PropertyTester {
+
+	public DbConnectionTester() {
+	}
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		AnalysisDatabase database = (AnalysisDatabase)receiver;
+		if ("isConnected".equals(property)) {
+			return database.isConnected();
+		}
+		else if ("isNotConnected".equals(property)) {
+			return !database.isConnected();
+		}
+		return false;
+	}
+
+}
