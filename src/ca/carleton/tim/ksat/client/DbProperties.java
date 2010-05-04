92ca7929c2204d0ca2199d615b04b5dd6a184be8
mnorman <michael.norman@oracle.com>
2010-05-04 12:14:05 -0400

    missed some files last commit

---
+++ src/ca/carleton/tim/ksat/client/DbProperties.java
@@ -0,0 +1,43 @@
+/*
+ * This software is licensed under the terms of the ISC License.
+ * (ISCL http://www.opensource.org/licenses/isc-license.txt
+ * It is functionally equivalent to the 2-clause BSD licence,
+ * with language "made unnecessary by the Berne convention" removed).
+ * 
+ * Copyright (c) 2010, Mike Norman
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE
+ * USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ */
+package ca.carleton.tim.ksat.client;
+
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Text;
+
+public class DbProperties {
+
+	protected Text databaseNameText;
+	protected Text userNameText;
+	protected Text urlText;
+	protected Text passwordText;
+	protected Combo logLevelCombo;
+	
+    public DbProperties(Text databaseNameText, Text userNameText, Text urlText, Text passwordText,
+    	Combo logLevelCombo) {
+		this.databaseNameText = databaseNameText;
+		this.userNameText = userNameText;
+		this.urlText = urlText;
+		this.passwordText = passwordText;
+		this.logLevelCombo = logLevelCombo;
+    }
+}
